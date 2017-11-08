a74bc41b90532a4e3e3320aff3c3daca182a3de0
Jonathan Yantz <ijryantz@gmail.com>
2017-11-08 15:03:05 -0500

    added navigation information and added all of the required functions for the collection view

--- Save365/Controller/CardController.swift
+++ Save365/Controller/CardController.swift
@@ -14,6 +14,18 @@ class CardController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         super.viewDidLoad()
         
+        // Add a name for the view controller and remove the translucency.
+        navigationItem.title = "Save365"
+        navigationController?.navigationBar.isTranslucent = false
+        
+        // Create the title bar with a design and text.
+        let titleLabel = UILabel(frame: CGRect.init(x: view.frame.width/2 - 80, y: 0, width: 160, height: 40))
+        titleLabel.text = "Save365"
+        titleLabel.textColor = UIColor.white
+        titleLabel.font = titleLabel.font.withSize(18)
+        titleLabel.textAlignment = .center
+        navigationItem.titleView = titleLabel
+        
         // Add the collection view to the view controller.
         collectionView?.register(CardCell.self, forCellWithReuseIdentifier: "CardCell")
         collectionView?.backgroundColor = UIColor.clear
@@ -24,6 +36,42 @@ class CardController: UICollectionViewController, UICollectionViewDelegateFlowLa
         gl.locations = [0.0, 1.0]
         
         collectionView?.layer.addSublayer(gl)
+        
+    }
+    
+    var card: [Card] = [Card]()
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 3
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cardCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCell
+        
+        cardCell.card = card[indexPath.item]
+        return cardCell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = view.frame.width - 40
+        return CGSize(width: size, height: 60)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 0.0, left: 20.0, bottom: 0.0, right: 20.0)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Do something when a row is selected.
         
     }
     
